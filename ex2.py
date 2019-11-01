97347c3f9d59dfa9dc781ef980542101fd45c011
jlaw8504 <josh.lawrimore@gmail.com>
2019-11-01 09:25:56 -0400

    Corrected mistakes in ex2.py after completing exercise 2 study drills.

--- ex2.py
+++ ex2.py
@@ -1,4 +1,4 @@
-# A comment, this is so you can read you program later.
+# A comment, this is so you can read your program later.
 # Anything after the # is ignored by python.
 
 print("I could have code like this.") # and the comment after is ignored
@@ -6,4 +6,4 @@ print("I could have code like this.") # and the comment after is ignored
 # You can also use a comment to "disable" or comment out code:
 # print("This won't run.")
 
-print("This will rune.")
+print("This will run.")
