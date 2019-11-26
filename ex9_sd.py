ed3f750fb52a25aeff0eaa1b0b19d657f3090dd8
jlaw8504 <josh.lawrimore@gmail.com>
2019-11-26 16:18:04 -0500

    Commented on each line of ex9.py in ex9_sd.py

---
+++ ex9_sd.py
@@ -0,0 +1,25 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# create a string variable called days that contain the
+# abbreviations of the days of the week
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# create a string variable called months that contain
+# the abbreviations of the months of the year separated
+# newline character \n
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# print out the string "Here are the days: " concatenated
+# with the string variable days
+print("Here are the days: ", days)
+# print out the string "Here are the months: " concatenated
+# with the string variable months
+print("Here are the months: ", months)
+
+# print out a long string using triple quotes
+# I wonder if this is called a doc-string in this context?
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
