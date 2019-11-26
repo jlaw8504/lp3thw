840c33a4c3ed333a0ad9ce2c2f3d9fcdbd2be489
jlaw8504 <josh.lawrimore@gmail.com>
2019-11-26 16:19:49 -0500

    Hand-typed ex7_sd.py for excersize 7 study drill

---
+++ ex7_sd1.py
@@ -0,0 +1,40 @@
+# Print out text, "Mary had a little lamb."
+print("Mary had a little lamb.")
+# print out text, "Its fleece was white as snow" using format method
+print("Its fleece was white as {}.".format('snow'))
+# print out text, "And everywhere that Mary went."
+print("And everywhere that Mary went.")
+# print out "." ten times using multiplication operator
+print("." * 10) # what'd that do?
+
+# set end1 variable to the character C
+end1 = "C"
+# set end2 variable to the character h
+end2 = "h"
+# set end3 variable to the character e 
+end3 = "e"
+# set end4 variable to the character e
+end4 = "e"
+# set end5 variable to the character s
+end5 = "s"
+# set end6 variable to the character e
+end6 = "e"
+# set end7 variable to the character B 
+end7 = "B"
+# set end8 variable to the character u 
+end8 = "u"
+# set end9 variable to the character r 
+end9 = "r"
+# set end10 variable to the character g 
+end10 = "g"
+# set end11 variable to the character e 
+end11 = "e"
+# set end12 variable to the character r 
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# print out concatenation of end1 through end6
+# set end character to single space instead of newline
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# print out concatenation of end7 through end 12
+print(end7 + end8 + end9 + end10 + end11 + end12)
