64f06847e1e6324a9efc5f1f7f54c0c692bb598b
jlaw8504 <lawrimor@email.unc.edu>
2019-12-01 12:36:34 -0500

    Hand-typed ex11_sd3.py

---
+++ ex11_sd3.py
@@ -0,0 +1,4 @@
+name = input("What is your name?") 
+quest = input("What is your quest?")
+color = input("What is your favorite color?")
+print(f"So your name is {name}.\nYou seek to {quest}.\nYour favorite color is {color}.\nYou may cross!")
