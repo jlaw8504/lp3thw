465da759828cb69204038a905d9c1ed54a7ab450
jlaw8504 <josh.lawrimore@gmail.com>
2019-12-04 13:46:46 -0500

    Added ex10_sd3.py

---
+++ ex10_sd3.py
@@ -0,0 +1,15 @@
+tabby_cat = "{}I'm tabbed in.".format('\t')
+persian_cat = "I'm split{}on a line.".format('\n')
+backslash_cat = "I'm {} a {} cat.".format('\\', '\\')
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
