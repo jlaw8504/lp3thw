eca2ef7ea37979f9b97288e3828d183c90e09b32
Josh <josh@Colleens-MBP.lan>
2019-12-30 16:44:55 -0600

    Hand-typed ex19_sd3.py

---
+++ ex19_sd3.py
@@ -0,0 +1,36 @@
+def print_lines(text, number):
+    text = text + "\n"
+    print(text * number)
+
+# first
+print_lines('dance', 3)
+
+# second
+text = 'dance'
+print_lines(text, 3)
+
+# third
+print_lines('sing', 2 + 1)
+
+# fourth
+print_lines('sing', 9)
+
+# fifth
+print_lines('laugh', 3)
+
+# sixth
+print_lines(text + ' again', 2 + 2)
+
+# seventh
+print_lines(text + ' and sing', 5)
+
+# eighth
+print_lines('rumble', 4)
+
+# ninth
+print_lines('rumble', 4 - 1)
+
+# tenth
+number = 1
+print_lines('rumble', number)
+
